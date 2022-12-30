2cf4ee328801b6cf51de841f6f97a656db7956e1
Víctor Muñoz Ramírez <vicmunram@alum.us.es>
2022-12-30 17:22:50 +0100

    Commit prueba

--- Assets/Scripts/NPCs/Enemies/Guardian.cs
+++ Assets/Scripts/NPCs/Enemies/Guardian.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Guardian : Enemy
 {
+    //Commit
     [SerializeField] private float distancing;
     [SerializeField] private float attackRate;
     private bool attacking;
